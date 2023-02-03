f2406296a1188d33dd2954ba58f64d732f8e2023
Alvin Kimata <alvinkimata@gmail.com>
2023-02-03 09:31:05 +0300

    Configuring ASP.Net Core request pipeline

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,20 +27,36 @@ namespace EmployeeManagement
 
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: Incoming request.");
+                await next();
+                logger.LogInformation("MW1: Outgoing response..");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming request.");
+                await next();
+                logger.LogInformation("MW2: Outgoing response..");
+            });
+
+
             app.Run(async (context) =>
                 {
                     await context.Response
-                        .WriteAsync(_config["MyKey"]);
+                        .WriteAsync("M3: Request handled and response produced.");
+                    logger.LogInformation("MW3: Outgoing response.");
                 });
         }
-
 
      }
 }
