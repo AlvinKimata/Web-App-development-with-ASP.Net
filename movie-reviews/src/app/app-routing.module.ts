7b215579cdac1194db6dc9fa0104506b17bfbd6c
Alvin Kimata <alvinkimata@gmail.com>
2023-02-08 16:30:30 +0300

    routing configuration

--- movie-reviews/src/app/app-routing.module.ts
+++ movie-reviews/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'movie', component: MovieComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
