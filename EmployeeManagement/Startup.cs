7ef5f2373f97eefb20bc54c9a493454da090666f
Alvin Kimata <alvinkimata@gmail.com>
2023-02-22 10:21:56 +0300

    update existing changes

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -1,9 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
---
+++ WebApplication1/Model/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class AuthDbContext: IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options): base(options)
+    }
+}
