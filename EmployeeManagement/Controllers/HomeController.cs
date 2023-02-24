4a2954c859e8832854aeec48696455f4197e0a80
Alvin Kimata <alvinkimata@gmail.com>
2023-02-24 14:40:29 +0300

    Passing data using viewdata

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -18,7 +18,9 @@ namespace EmployeeManagement.Controllers
         public ViewResult Details()
         {
             Employee model = _employeeRepository.GetEmployee(1);
-            return View("../Test/Update");
+            ViewData["employee"] = model;
+            ViewData["pageTitle"] = "Employee Details";
+            return View();
         }
     }
 }
