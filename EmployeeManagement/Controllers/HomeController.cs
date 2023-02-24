cd2069277028a0ee092383e69313534343118b28
Alvin Kimata <alvinkimata@gmail.com>
2023-02-24 09:15:35 +0300

    Controllers in ASP.Net

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -14,5 +14,11 @@ namespace EmployeeManagement.Controllers
         {
             return _employeeRepository.GetEmployee(1).Name;
         }
+
+        public JsonResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return Json(model);
+        }
     }
 }
