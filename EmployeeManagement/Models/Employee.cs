45068149cb53bb577242300cacecfd61042a3da0
Alvin Kimata <alvinkimata@gmail.com>
2023-03-02 08:00:02 +0300

    asp validation error summary

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -11,6 +11,7 @@ namespace EmployeeManagement.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(50, ErrorMessage ="Name cannot exceed 50 characters.")]
         public string Name { get; set; }
         [Required]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
