cf26da674dc5fd53c4a0587061313192fc433f35
Alvin Kimata <alvinkimata@gmail.com>
2023-03-02 10:58:12 +0300

    add operations in employeerepo interface

--- EmployeeManagement/Models/IEmployeeRepository.cs
+++ EmployeeManagement/Models/IEmployeeRepository.cs
@@ -10,5 +10,8 @@ namespace EmployeeManagement.Models
         Employee GetEmployee(int Id);
         IEnumerable<Employee> GetAllEmployees();
         Employee Add(Employee employee);
+        Employee Update(Employee employeeChanges);
+
+        Employee Delete(int id);
     }
 }
