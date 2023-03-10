8b448e6288eb1f84c7b58a4359c6859f08a5ebf5
Alvin Kimata <alvinkimata@gmail.com>
2023-03-10 08:20:17 +0300

    password complexity

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -30,8 +30,11 @@ namespace EmployeeManagement
             var connection = _config.GetConnectionString("EmployeeDBConnection");
             services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 10;
+                options.Password.RequiredUniqueChars = 3;
+            }).AddEntityFrameworkStores<AppDbContext>();
 
             services.AddMvc();
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
