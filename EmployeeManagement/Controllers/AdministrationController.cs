4c191364229254d1a541de91a5d2a393917185ea
Alvin Kimata <alvinkimata@gmail.com>
2023-03-15 10:51:07 +0300

    authorize user who is both admin and regular user

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -10,7 +10,8 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
 {
-    [Authorize(Roles = "Admin, User")]
+    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "User")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
