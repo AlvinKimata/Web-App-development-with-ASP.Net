1a75c574637d87b95d1682cb3366e075b8bbe12d
Alvin Kimata <alvinkimata@gmail.com>
2023-03-16 09:36:15 +0300

    inline delete confirmation

--- EmployeeManagement/wwwroot/js/CustomScript.js
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -1,1 +1,15 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isDeleteClicked) {
+        //Hide the delete span element.
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    }
+    else {
+        //Show the delete span element.
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
